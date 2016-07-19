b2d21026d53fbf095c30ce67356aea3076aa6487
kirandash <thekirankumardash@gmail.com>
2016-07-19 22:42:10 +0530

    2.3. Activating Multisite - END 02 - Getting Started With Multisite

--- wp-config.php
+++ wp-config.php
@@ -81,6 +81,16 @@ define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
+// Multisite
+define('WP_ALLOW_MULTISITE', true);
+
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'localhost');
+define('PATH_CURRENT_SITE', '/wpmultisite/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
